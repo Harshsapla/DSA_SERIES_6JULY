28febe60692244014dcb762b3819c33c5610d5a6
Harsh Yadav <harshsapla03@gmail.com>
2025-07-17 23:38:34 +0530

    update

--- script.js
+++ script.js
@@ -453,3 +453,30 @@
 // // Using alternative approach
 // console.log("Alternative Test 1:", mergeAlternative([[1,3],[2,6],[8,10],[15,18]]));
 // // Expected: [[1,6],[8,10],[15,18]]
+Easy
+Topics
+premium lock icon
+Companies
+A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+Given a string s, return true if it is a palindrome, or false otherwise.
+
+ 
+
+Example 1:
+
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+Example 2:
+
+Input: s = "race a car"
+Output: false
+Explanation: "raceacar" is not a palindrome.
+Example 3:
+
+Input: s = " "
+Output: true
+Explanation: s is an empty string "" after removing non-alphanumeric characters.
+Since an empty string reads the same forward and backward, it is a palindrome.
+ 
