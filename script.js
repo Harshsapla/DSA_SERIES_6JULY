a2465e476d48df1a96facc2286e9fd15dac19fde
Harsh Yadav <harshsapla03@gmail.com>
2025-07-20 22:02:06 +0530

    update

--- script.js
+++ script.js
@@ -596,3 +596,17 @@
 
 //   return dummyHead.next;
 // }
+var isPalindrome = function(x) {
+    // Negative numbers or numbers ending in 0 (but not 0 itself) can't be palindrome
+    if (x < 0 || (x % 10 === 0 && x !== 0)) return false;
+
+    let reversed = 0;
+    while (x > reversed) {
+        reversed = reversed * 10 + x % 10;
+        x = Math.floor(x / 10);
+    }
+
+    // For even digit numbers, x === reversed
+    // For odd digit numbers, x === Math.floor(reversed / 10)
+    return x === reversed || x === Math.floor(reversed / 10);
+};
